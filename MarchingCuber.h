c3152b605736fa2512cd8b255c59f35888de84f7
ulfbiallas <ulfbiallas@googlemail.com>
2015-03-21 23:27:52 +0100

    created marching cubes class

---
+++ MarchingCuber.h
@@ -0,0 +1,55 @@
+#ifndef MARCHINGCUBER_H
+#define MARCHINGCUBER_H
+
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#include "datatypes.h"
+
+
+
+using namespace std;
+
+class Chunk;
+
+class MarchingCuber {
+	
+    public:      
+
+		struct TRIANGLE {
+
+			Vec3f p[3], n[3];
+
+			TRIANGLE() {
+				p[0] = Vec3f();
+				p[1] = Vec3f();
+				p[2] = Vec3f();
+				n[0] = Vec3f();
+				n[1] = Vec3f();
+				n[2] = Vec3f();
+			};
+		};
+
+		struct GRIDCELL {
+		   Vec3f p[8];
+		   Vec3f n[8];
+		   float val[8];
+		};
+
+
+
+		MarchingCuber(); 
+		vector<TRIANGLE> marchingCubes(float **voxels, Vec3f pos, int dimX, int dimY, int dimZ, float h, float isolevel);
+
+
+
+	private:
+		
+		Vec3f getGradient(float **voxels, int dimX, int dimY, int dimZ, int ix_, int iy_, int iz_);
+		Vec3f VertexInterp(float isolevel, Vec3f p1, Vec3f p2, float valp1, float valp2);
+		int Polygonise(GRIDCELL grid, float isolevel, TRIANGLE *triangles);
+		int idx(int dimX, int dimY, int ix_, int iy_, int iz_);	
+
+};
+
+#endif
